846f91c23b3526cacd3342e18154c73205dfef4b
salaaad2 <salaaad2@gitlab.com>
2021-05-26 22:03:53 +0200

    references references references

--- inc/Params.hpp
+++ inc/Params.hpp
@@ -31,17 +31,34 @@ public:
 		Params();
 		Params(int ac, std::vector<std::string>);
 		~Params();
-		std::string getHost() const {return _host;}
-		void setHost(std::string new_host) { _host = new_host;}
-		std::string getPwdNetwork() const {return _password_network;}
-		void setPwdNetwork(std::string new_pwd) { _password_network = ft_atoi(new_pwd.c_str());}
-		std::string getPwd() const {return _password;}
-		void setPwd(std::string new_pwd) { _password = ft_atoi(new_pwd.c_str());}
 
-		int		getPortNetwork() const {return _port_network;}
-		void setPortNetwork(std::string new_port) { _port_network = ft_atoi(new_port.c_str());}
-		int		getPort() const {return _port;}
-		void setPort(std::string new_port) { _port = ft_atoi(new_port.c_str());}
-		bool	isnew() const {return _isnew;}
-		void setIsNew(bool is_new) { _isnew = is_new;}
+		std::string getHost() const
+		{return _host;}
+		void setHost(std::string & new_host)
+		{ _host = new_host;}
+
+		std::string getPwdNetwork() const
+		{return _password_network;}
+		void setPwdNetwork(std::string & new_pwd)
+		{ _password_network = ft_atoi(new_pwd.c_str());}
+
+		std::string getPwd() const
+		{return _password;}
+		void setPwd(std::string & new_pwd)
+		{ _password = ft_atoi(new_pwd.c_str());}
+
+		int		getPortNetwork() const
+		{return _port_network;}
+		void setPortNetwork(std::string & new_port)
+		{ _port_network = ft_atoi(new_port.c_str());}
+
+		int		getPort() const
+		{return _port;}
+		void setPort(std::string & new_port)
+		{ _port = ft_atoi(new_port.c_str());}
+
+		bool	isnew() const
+		{return _isnew;}
+		void setIsNew(bool & is_new)
+		{ _isnew = is_new;}
 };
