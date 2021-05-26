4c15d6cffb7556196d915144f63ff4428f5b476b
salaaad2 <salaaad2@github.com>
2021-05-26 15:22:42 +0200

    found the bug

--- inc/Params.hpp
+++ inc/Params.hpp
@@ -28,39 +28,9 @@ private:
 
 public:
 
-	Params(int ac, std::vector<std::string> av)
-
-	{
-		if (ac != 3 && ac != 4)
-		{
-			std::cout << "wrong parameters\n";
-			return ;
-		}
-
-		if (ac == 4)
-		{
-			size_t fst = av[0].find(':');
-			size_t scd = av[0].find(':', fst + 1);
-			size_t no = av[0].find(':', scd + 1);
-
-			if (fst == std::string::npos || scd == std::string::npos || no != std::string::npos)
-				return ;
-
-			_host =  av[0].substr(0, fst);
-			_port_network =  ft_atoi(av[0].substr(fst + 1, scd - fst - 1).c_str());
-			_password_network = av[0].substr(scd + 1, av[0].length() - scd);
-			_port = ft_atoi(av[1].c_str());
-			_password = av[2];
-			_isnew = false;
-		}
-		else
-		{
-			_port = ft_atoi(av[0].c_str());
-			_password = av[1];
-			_isnew = true;
-		}
-	}
-
+		Params();
+		Params(int ac, std::vector<std::string>);
+		~Params();
 		std::string getHost() const {return _host;}
 		void setHost(std::string new_host) { _host = new_host;}
 		std::string getPwdNetwork() const {return _password_network;}
